56f63af5fbb4286923e0b6d43b3d2a310f8576e3
ValentinLutz <allround.email@gmail.com>
2024-01-01 21:43:22 +0100

    Fix testing helper functions

--- libraries/testutil/serde.go
+++ libraries/testutil/serde.go
@@ -7,25 +7,34 @@ import (
 )
 
 func CreateBody(t *testing.T, path string) io.Reader {
+	t.Helper()
+
 	reader, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("failed to open file, %v", err)
 	}
+
 	return reader
 }
 
 func ReadBody(t *testing.T, reader io.Reader) []byte {
+	t.Helper()
+
 	bytes, err := io.ReadAll(reader)
 	if err != nil {
 		t.Fatalf("failed to decode input, %v", err)
 	}
+
 	return bytes
 }
 
 func ReadFile(t *testing.T, path string) []byte {
+	t.Helper()
+
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		t.Fatalf("failed to read file from path %v, %v", path, err)
 	}
+
 	return bytes
 }
