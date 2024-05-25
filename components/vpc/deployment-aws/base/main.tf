23eda5cc3a6af6480ef2f46515126dfe8a319b5d
ValentinLutz <allround.email@gmail.com>
2024-05-25 17:27:48 +0200

    Disable vpc nat gateways

--- components/vpc/deployment-aws/base/main.tf
+++ components/vpc/deployment-aws/base/main.tf
@@ -41,33 +41,35 @@ resource "aws_subnet" "private" {
   availability_zone = each.value.availability_zone
 }
 
-resource "aws_eip" "private" {
-  for_each = aws_subnet.private
-
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "private" {
-  for_each = aws_subnet.private
-
-  allocation_id = aws_eip.private[each.key].id
-  subnet_id     = each.value.id
-}
-
-resource "aws_route_table" "private" {
-  for_each = aws_nat_gateway.private
-
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = each.value.id
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  for_each = aws_subnet.private
-
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.private[each.key].id
-}
+# Commented out because $$$$$$
+#
+# resource "aws_eip" "private" {
+#   for_each = aws_subnet.private
+#
+#   domain = "vpc"
+# }
+#
+# resource "aws_nat_gateway" "private" {
+#   for_each = aws_subnet.private
+#
+#   allocation_id = aws_eip.private[each.key].id
+#   subnet_id     = each.value.id
+# }
+#
+# resource "aws_route_table" "private" {
+#   for_each = aws_nat_gateway.private
+#
+#   vpc_id = aws_vpc.vpc.id
+#
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = each.value.id
+#   }
+# }
+#
+# resource "aws_route_table_association" "private" {
+#   for_each = aws_subnet.private
+#
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.private[each.key].id
+# }
